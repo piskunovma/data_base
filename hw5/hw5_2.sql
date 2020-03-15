d71019b1665738b5829124f19ef6e808e732b3a2
Михаил Пискунов <piskunov.m.a@gmail.com>
2020-03-15 16:09:37 +0300

    hw5_2 update

--- hw5/hw5_2.sql
+++ hw5/hw5_2.sql
@@ -3,4 +3,18 @@
  помещались значения в формате "20.10.2017 8:10". 
  Необходимо преобразовать поля к типу DATETIME, сохранив введеные ранее значения.
 */
+use vk_test;
 
+-- UPDATE 
+
+-- ALTER TABLE vk_test.users MODIFY COLUMN updated_at DATETIME NULL;
+-- ALTER TABLE vk_test.users MODIFY COLUMN created_at DATETIME NULL;
+
+SELECT
+  id,
+  name,
+  birthday_at,
+  DATETIME(created_at) created_at,
+  DATETIME(updated_at) updated_at
+FROM
+users;
